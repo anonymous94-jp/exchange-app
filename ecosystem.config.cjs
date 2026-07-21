874003436ce0a9bd7fd7a4f9a02a0d0f0fb2348f
Melino <anhpham@PHAMs-MacBook-Pro.local>
2026-07-21 19:49:21 +0900

    Remove ignored folder from Git tracking

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: "exchange-app",
+
+            cwd: "/root/exchange-app",
+
+            script: "./src/index.js",
+
+            autorestart: false,
+
+            watch: false,
+
+            max_memory_restart: "1200M"
+        }
+    ]
+};
